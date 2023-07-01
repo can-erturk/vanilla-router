840e70712237dd3f5eaaa31643fa7fabbf03c424
meecan <cannhann42@gmail.com>
2023-07-02 00:31:53 +0300

    HTML components defined in router.js

---
+++ src/router.js
@@ -0,0 +1,5 @@
+import { RouterPage, RouterRoot } from './modules/routerDom.js'
+
+// Defining HTML elements that can be used as <router-root> and <router-page>
+customElements.define('router-page', RouterPage)
+customElements.define('router-root', RouterRoot)
