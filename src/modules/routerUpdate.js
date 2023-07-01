74b09b908a2b9a86feb768af8462a38f2cdcc242
meecan <cannhann42@gmail.com>
2023-07-02 02:05:04 +0300

    The content inside the <head> tag is now customizable for each page.

--- src/modules/routerUpdate.js
+++ src/modules/routerUpdate.js
@@ -1,10 +1,35 @@
+import Utils from './Utils.js';
+
+
 class RouterUpdate {
+
+    static headUpdate = (page) => {
+        const regex = /<head>([^]*?)<\/head>/i
+        const matches = page.match(regex)
+
+        if (!matches || matches.length <= 1) {
+            return
+        }
+
+        const { defaultHead } = Utils.options
+        const newHead = matches[1].trim()
+        document.head.innerHTML = `${defaultHead} ${newHead}`
+    }
+
+
     static contentUpdate = async (page) => {
         const response = await fetch(page)
         const newPage = await response.text()
 
-        document.querySelector('#root').innerHTML = newPage
+        const regexForHead = /<head>.*<\/head>/s
+        
+        const newHtml = newPage.replace(regexForHead, '')
+        document.querySelector('#root').innerHTML = newHtml
+
+        this.headUpdate(newPage)
     }
+
 }
+
 
 export default RouterUpdate
