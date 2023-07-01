c45c31b12cb0e0b83e7cabfa07abfe5cb140d205
meecan <cannhann42@gmail.com>
2023-07-02 01:14:28 +0300

    The necessary events for routing started to be listened in router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,12 @@
 import { RouterPage, RouterRoot } from './modules/routerDom.js'
+import RouterHandle from './modules/routerHandle.js'
+
 
 // Defining HTML elements that can be used as <router-root> and <router-page>
 customElements.define('router-page', RouterPage)
 customElements.define('router-root', RouterRoot)
+
+
+// Listen events for routing
+window.addEventListener('popstate', RouterHandle.handle)
+window.addEventListener('DOMContentLoaded', RouterHandle.handle)
