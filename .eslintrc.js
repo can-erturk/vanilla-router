aaf9c75f5e8a1fcc999e5060aa5e8d2906df6f45
meecan <cannhann42@gmail.com>
2023-08-01 18:38:04 +0300

    Added ESLint with custom rules.

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'rules': {
+		'max-len': ['error', {'code': 120}],
+		'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+		'indent': ['error', 4],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
