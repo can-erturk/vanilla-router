10a9538530ee761823aca446c90808670dbba982
meecan <cannhann42@gmail.com>
2023-08-01 14:54:12 +0300

    Added simple usage example.

---
+++ examples/pages/assets/js/github.js
@@ -0,0 +1,2 @@
+console.clear()
+console.log('%cGithub page script injected.', 'font-size:18px;font-family:system-ui;padding:20px')
---
+++ examples/pages/assets/js/home.js
@@ -0,0 +1,2 @@
+console.clear()
+console.log('%cHome page script injected.', 'font-size:18px;font-family:system-ui;padding:20px')
